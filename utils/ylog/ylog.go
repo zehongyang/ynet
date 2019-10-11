b8f09569864f25abbeb7e68c8fa26fd7509835a7
yangzehong <yangzehong@zolbon.com>
2019-10-11 16:04:50 +0800

    修改日志

--- utils/ylog/ylog.go
+++ utils/ylog/ylog.go
@@ -14,7 +14,7 @@ var (
 //初始化log
 func init()  {
 	//将日志记录到runtime/log目录下
-	var logDir = "./runtime/log"
+	var logDir = "./runtime/log/"
 	_, err := os.Stat(logDir)
 	//如果运用下没日志目录则创建
 	if os.IsNotExist(err) {
@@ -30,7 +30,7 @@ func init()  {
 		fmt.Println("init log err:",err)
 		return
 	}
-	logger = log.New(file, "logger: ", log.Lshortfile)
+	logger = log.New(file, "logger: ", log.LstdFlags)
 }
 
 //记录信息到日志
